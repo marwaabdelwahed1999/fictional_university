896ffd027ddfbed1cc1041d767607d2e45235b1c
Your Name <meroabdelwahed477@gmail.com>
2024-01-14 03:44:30 +0200

    sextion _3

---
+++ wp-content/themes/fictional-university-theme/index.php
@@ -0,0 +1,27 @@
+<h1><?php bloginfo('nmae');                ?>
+<!-- website name -->
+</h1>
+
+<!-- description -->
+<p><?php bloginfo('description');                ?>
+</p>
+
+<p><?php bloginfo('url');                ?>
+</p>
+
+<!-- looping and arrays -->
+<?php 
+$names = array('Mero','nody','zezo','soso','mohamed');
+
+$count = 0;
+while ($count < count($names)) {
+    echo "<li>$names[$count]</li>" ;
+   $count++;
+}
+
+
+
+
+
+
+?>
